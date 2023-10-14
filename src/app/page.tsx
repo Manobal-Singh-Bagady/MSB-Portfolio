df3ab869524288e2d1a9930740d0c696f417fb62
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-10-15 05:19:49 +0530

    add hero section to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import HeroSection from '@/components/HeroSection'
 import Image from 'next/image'
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	NavigationMenuViewport,
+} from '@/components/ui/navigation-menu'
+import Navbar from '@/components/Navbar'
 
 export default function Home() {
-	return <main className=''></main>
+	return (
+		<main className='flex min-h-screen flex-col'>
+			<Navbar />
+
+			<div className='container mx auto py-4 px-12'>
+				<HeroSection />
+			</div>
+		</main>
+	)
 }
